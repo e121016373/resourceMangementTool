addbfe266f2c155abb1412f74df8418017c771c5
Eric Ly <lye@AE.CA>
2020-01-08 08:49:31 -0800

    update database tables and seed script

--- src/svc-dotnetcore3/Database/dbo/Tables/Expenses.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Expenses] (
-    [Id]          INT           IDENTITY (1, 1) NOT NULL,
-    [Description] NVARCHAR (50) NULL,
-    [Date]        DATETIME      NULL,
-    [Value]       INT           NOT NULL,
-    CONSTRAINT [PK_Expenses] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
