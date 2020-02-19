02af811a8a1c24bac57bbc073a536b25c5dfb955
winton <wenhong.winton.zhang@gmail.com>
2020-02-18 21:54:18 -0800

    admin

--- src/ui-react-client/src/components/login/LoginPage.js
+++ src/ui-react-client/src/components/login/LoginPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const LoginPage = () => {
   return (
-    <>
+    <div>
       <h1>LoginPage</h1>
       <p>Loginpage</p>
-    </>
+    </div>
   );
 };
 export default LoginPage;
