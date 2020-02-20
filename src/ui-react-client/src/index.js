65b9916e216f5b7ae3f971fded2e4759b5bda045
winton <wenhong.winton.zhang@gmail.com>
2020-02-20 15:25:01 -0800

    home page style

--- src/ui-react-client/src/index.js
+++ src/ui-react-client/src/index.js
@@ -1,18 +1,18 @@
-import { runWithAdal } from 'react-adal';
-import { authContext, headers } from './config/adalConfig';
+// import { runWithAdal } from 'react-adal';
+// import { authContext, headers } from './config/adalConfig';
 
-const DO_NOT_LOGIN = false;
+// const DO_NOT_LOGIN = false;
 
-function run() {
-  runWithAdal(
-    authContext,
-    () => {
-      console.log('the headers is ' + headers.Authorization);
-      // eslint-disable-next-line
-      require('./indexApp.js');
-    },
-    DO_NOT_LOGIN,
-  );
-}
-//run();
+// function run() {
+//   runWithAdal(
+//     authContext,
+//     () => {
+//       console.log('the headers is ' + headers.Authorization);
+//       // eslint-disable-next-line
+//       require('./indexApp.js');
+//     },
+//     DO_NOT_LOGIN,
+//   );
+// }
+// //run();
 require('./indexHome.js');
--- src/ui-react-client/src/indexHome.js
+++ src/ui-react-client/src/indexHome.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './indexApp.css';
-import App from './components/App';
+import './css/home.css';
 import configureStore from './redux/configureStore';
 import { Provider } from 'react-redux';
-import AdminPage from './components/admin/AdminPage';
 import { runWithAdal } from 'react-adal';
-import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import { authContext, headers } from './config/adalConfig';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 const DO_NOT_LOGIN = false;
 
 render(
   <Provider store={store}>
-    <button onClick={authentication}>user</button>
-    <button onClick={admin}>admine</button>
+    <div className="main">
+      <Button
+        size="lg"
+        variant="outline-secondary"
+        className="my-button"
+        onClick={authentication}
+      >
+        user
+      </Button>
+      <Button
+        size="lg"
+        variant="outline-secondary"
+        className="my-button"
+        onClick={admin}
+      >
+        admin
+      </Button>
+    </div>
   </Provider>,
   document.getElementById('root'),
 );
