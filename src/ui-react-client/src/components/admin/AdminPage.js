dcee66495147dfdf8371c95cb0db2a52ac55262c
jungwookjang <b8n2b@ugrad.cs.ubc.ca>
2020-02-24 13:08:23 -0800

    Jungwook Jang text Commit

--- src/ui-react-client/src/components/admin/AdminPage.js
+++ src/ui-react-client/src/components/admin/AdminPage.js
@@ -1,42 +1,28 @@
 import React from 'react';
 import { Form } from 'simple-react-form';
+import ReactDOM from "react-dom";
+import UserTable from "../subComponents/UserTalbe";
+import LocationTable from "../subComponents/LocationTable";
+import SkillTable from "../subComponents/SkillTable";
+import DisciplineTable from "../subComponents/DisciplineTable";
+import Button from 'react-bootstrap/Button';
+import  "../../css/admin.css";
+
 const AdminPage = () => {
   return (
-    <div className="container">
-      <h2>Administrator</h2>
-      <p>You can either remover User or update User profile here.</p>
-      <div className="search-container"></div>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>User Nmae</th>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>jjang14</td>
-            <td>Jungwook</td>
-            <td>Jang</td>
-            <td>N'Vancouver</td>
-          </tr>
-          <tr>
-            <td>wintondDaGod</td>
-            <td>Winton</td>
-            <td>Einstein</td>
-            <td>N'Edmonton</td>
-          </tr>
-          <tr>
-            <td>bob11</td>
-            <td>Bob</td>
-            <td>Ghoush</td>
-            <td>N'Toronto</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+     <div className="main" class="d-flex p-3 bg secondary">
+         <div class="leftSide">
+             <UserTable/>
+             <LocationTable/>
+         </div>
+         <div class = "main">
+             <SkillTable/>
+         </div>
+         <div class = "rightSide">
+             <DisciplineTable/>
+         </div>
+     </div>
+
   );
 };
 export default AdminPage;
