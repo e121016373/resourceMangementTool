f6d1434619bdc4e424e56a2cac96d056d07f4ffe
winton <wenhong.winton.zhang@gmail.com>
2020-02-25 15:23:32 -0800

    fix

--- src/ui-react-client/src/components/admin/AdminPage.js
+++ src/ui-react-client/src/components/admin/AdminPage.js
@@ -10,38 +10,18 @@ import '../../css/admin.css';
 
 const AdminPage = () => {
   return (
-<<<<<<< HEAD
     <div className="main" class="d-flex p-3 bg secondary">
-      <div className="main">
-        <div class="sub">
-          <UserTable />
-        </div>
-        <div className="sub">
-          <SkillTable />
-        </div>
-        <div className="sub">
-          <DisciplineTable />
-        </div>
-        <div className="sub">
-          <LocationTable />
-        </div>
+      <div class="leftSide">
+        <UserTable />
+        <LocationTable />
+      </div>
+      <div class="center">
+        <SkillTable />
+      </div>
+      <div class="rightSide">
+        <DisciplineTable />
       </div>
     </div>
-=======
-     <div className="main" class="d-flex p-3 bg secondary">
-         <div class="leftSide">
-             <UserTable/>
-             <LocationTable/>
-         </div>
-         <div class = "center">
-             <SkillTable/>
-         </div>
-         <div class = "rightSide">
-             <DisciplineTable/>
-         </div>
-     </div>
-
->>>>>>> 89fcf83619e6bf267556d178e8ff40fd7baa870b
   );
 };
 export default AdminPage;
