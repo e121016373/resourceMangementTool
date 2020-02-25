a9cf53731fa09106e51bc24da7ff5b4c4b14cfe3
winton <wenhong.winton.zhang@gmail.com>
2020-02-25 08:24:05 -0800

    kk

--- src/ui-react-client/src/components/admin/AdminPage.js
+++ src/ui-react-client/src/components/admin/AdminPage.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Form } from 'simple-react-form';
-import ReactDOM from "react-dom";
-import UserTable from "../subComponents/UserTalbe";
-import LocationTable from "../subComponents/LocationTable";
-import SkillTable from "../subComponents/SkillTable";
-import DisciplineTable from "../subComponents/DisciplineTable";
+import ReactDOM from 'react-dom';
+import UserTable from '../subComponents/UserTalbe';
+import LocationTable from '../subComponents/LocationTable';
+import SkillTable from '../subComponents/SkillTable';
+import DisciplineTable from '../subComponents/DisciplineTable';
 import Button from 'react-bootstrap/Button';
-import  "../../css/admin.css";
+import '../../css/admin.css';
 
 const AdminPage = () => {
   return (
-     <div className="main" class="d-flex p-3 bg secondary">
-         <div class="leftSide">
-             <UserTable/>
-             <LocationTable/>
-         </div>
-         <div class = "main">
-             <SkillTable/>
-         </div>
-         <div class = "rightSide">
-             <DisciplineTable/>
-         </div>
-     </div>
-
+    <div className="main" class="d-flex p-3 bg secondary">
+      <div className="main">
+        <div class="sub">
+          <UserTable />
+        </div>
+        <div className="sub">
+          <SkillTable />
+        </div>
+        <div className="sub">
+          <DisciplineTable />
+        </div>
+        <div className="sub">
+          <LocationTable />
+        </div>
+      </div>
+    </div>
   );
 };
 export default AdminPage;
