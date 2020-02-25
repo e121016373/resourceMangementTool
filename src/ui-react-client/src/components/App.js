1371fb54545397b6b26bec62713133b738230543
eddhuang <eddhuang@students.cs.ubc.ca>
2020-02-25 00:52:52 -0800

    remove comments

--- src/ui-react-client/src/components/App.js
+++ src/ui-react-client/src/components/App.js
@@ -16,19 +16,13 @@ const App = () => {
       <Header />
       <Switch>
         {/*All our Routes goes here!*/}
-        {/* <Route exact path="/users" component={HomePage} />
-        <Route path="/users/users" component={UsersPage} />
-        <Route path="/users/projects" component={ProjectsPage} />
-        <Route path="/users/locations" component={LocationsPage} /> */}
-
         <Route exact path="/" component={HomePage} />
         <Route path="/users" component={UsersPage} />
         <Route path="/projects/:project/:user" component={UserInfoPage}/>
         <Route path="/projects/:project" component={ProjectInfoPage} />
         <Route path="/projects" component={ProjectsPage} />
         <Route path="/locations" component={LocationsPage} />
-        {/* add admine component */}
-        {/* <Route path="/users/admin" component={AdminPage} /> */}
+        {/* add admin component */}
         <Route path="/admin" component={AdminPage} />
         <Route component={PageNotFound} />
       </Switch>
