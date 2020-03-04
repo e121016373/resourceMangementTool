a51f8683e92f482994cd32079fb3b80476b55dda
Bob Ghosh <ghosh.bob@gmail.com>
2020-03-04 15:48:28 -0800

    React Client Code Correction

--- src/ui-react-client/src/components/admin/AdminPage.js
+++ src/ui-react-client/src/components/admin/AdminPage.js
@@ -10,14 +10,14 @@ import '../../css/admin.css';
 
 const AdminPage = () => {
   return (
-    <div className="main" class="d-flex p-3 bg secondary">
-      <div class="leftSide">
+    <div className="d-flex p-3 bg secondary">
+      <div className="leftSide">
         <UserTable />
         <LocationTable />
       </div>
-      <div class="center">
+      <div className="center">
       </div>
-      <div class="rightSide">
+      <div className="rightSide">
         <SkillTable />
         <DisciplineTable />
       </div>
