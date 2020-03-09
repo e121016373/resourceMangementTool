aa75cfb1774ed8be1680fc3008de66ea69b79072
shuaiqiz <shuaiqizhangwu@gmail.com>
2020-03-09 13:26:56 -0700

    feature:expandable project table

--- src/ui-react-client/src/components/projects/ProjectInfoPage.js
+++ src/ui-react-client/src/components/projects/ProjectInfoPage.js
@@ -1,54 +1,72 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 
-let project1 = {id:1,name:"Project1", status:"Active", location:"Vancouver", startDate:"20190401", endDate:"20200201",utilization:"100%"};
-let project2 = {id:2,name:"Project2", status:"Active", location:"Toronto", startDate:"20190821", endDate:"20191105",utilization:"60%"};
-let project3 = {id:3,name:"Project3", status:"Inactive", location:"Richmond", startDate:"20200121", endDate:"20211010",utilization:"0%"};
+let project1 = {
+    id: 1,
+    name: "Project1",
+    status: "Active",
+    location: "Vancouver",
+    startDate: "20190401",
+    endDate: "20200201",
+    utilization: "100%"
+};
+let project2 = {
+    id: 2,
+    name: "Project2",
+    status: "Active",
+    location: "Toronto",
+    startDate: "20190821",
+    endDate: "20191105",
+    utilization: "60%"
+};
+let project3 = {
+    id: 3,
+    name: "Project3",
+    status: "Inactive",
+    location: "Richmond",
+    startDate: "20200121",
+    endDate: "20211010",
+    utilization: "0%"
+};
 let projects = [project1, project2, project3];
 
-let user1 = {id:1,name:"user1",utilization:"30%"}
-let user2 = {id:2,name:"user2",utilization:"40%"}
-let user3 = {id:3,name:"user3",utilization:"0%"}
-let users = [user1,user2,user3]
+// let user1 = {id: 1, name: "user1", utilization: "30%"}
+// let user2 = {id: 2, name: "user2", utilization: "40%"}
+// let user3 = {id: 3, name: "user3", utilization: "0%"}
+// let users = [user1, user2, user3]
+//
 
-
-const ProjectInfoPage = () => {
-    const {project} = useParams();
+function ProjectInfoPage(props) {
+    // const {project} = useParams();
     // noinspection JSAnnotator
     return (
-        <>
-            <h1> {project.toString().toUpperCase()} DETAILS</h1>
-            <hr />
+        <div>
+            <h3> {props.project.name} DETAILS</h3>
+            <hr/>
             <table>
                 <tr>
-                    <td id="t02">
-                        <table>
-                            <tr><th id="t03" colSpan={2}>INFO</th></tr>
-                            <tr>
-                                <td>Status</td>
-                                <td>{project1.status}</td>
-                            </tr>
-                            <tr>
-                                <td>Location</td>
-                                <td>{project1.location}</td>
-                            </tr>
-                            <tr>
-                                <td>Start Date</td>
-                                <td>{project1.startDate}</td>
-                            </tr>
-                            <tr>
-                                <td>End Date</td>
-                                <td>{project1.endDate}</td>
-                            </tr>
-                        </table>
-                    </td>
-                    <td id="t02"><h1>UTILIZATION VISUALIZATION</h1></td>
+                    <th colSpan={2}>INFO</th>
+                </tr>
+                <tr>
+                    <td>Status</td>
+                    <td>{project1.status}</td>
+                </tr>
+                <tr>
+                    <td>Location</td>
+                    <td>{project1.location}</td>
+                </tr>
+                <tr>
+                    <td>Start Date</td>
+                    <td>{project1.startDate}</td>
+                </tr>
+                <tr>
+                    <td>End Date</td>
+                    <td>{project1.endDate}</td>
                 </tr>
             </table>
-            <br />
             <hr/>
 
-            <table id={"t01"}>
+            <table id="t01">
                 <thead>
                 <tr>
                     <th>Members</th>
@@ -58,28 +76,26 @@ const ProjectInfoPage = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {users.map(user => (
+                {props.users.map(user => (
                     <tr key={user.id}>
                         <td>
-                            <a href={"/projects/"+project.name+"/"+user.name}>{user.name}</a>
+                            {user.name}
                         </td>
                         {/*<td>{user.name}</td>*/}
                         <td>{user.utilization}</td>
-                        <td><button type="button">Edit</button></td>
-                        <td><button type="button">Delete</button></td>
+                        <td>
+                            <button type="button">Edit</button>
+                        </td>
+                        <td>
+                            <button type="button">Delete</button>
+                        </td>
                     </tr>
                 ))}
                 </tbody>
             </table>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-        </>
+        </div>
     );
 };
-
-
 
 
 export default ProjectInfoPage;
--- src/ui-react-client/src/components/projects/ProjectsPage.js
+++ src/ui-react-client/src/components/projects/ProjectsPage.js
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
 import '../../css/collapseStyle.css';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {loadProjects} from '../../redux/actions/projectsActions';
-import {loadLocations} from '../../redux/actions/locationsActions.js';
-import ProjectList from './ProjectList';
+import rd3 from 'react-d3-library';
+import ProjectInfoPage from "./ProjectInfoPage";
+
+// import {connect} from 'react-redux';
+// import PropTypes from 'prop-types';
+// import {loadProjects} from '../../redux/actions/projectsActions';
+// import {loadLocations} from '../../redux/actions/locationsActions.js';
+// import ProjectList from './ProjectList';
 
 // const _ProjectsPage = ({
 //   projects,
@@ -61,59 +64,106 @@ let project3 = {
     utilization: "0%"
 };
 let projects = [project1, project2, project3]
+let user1 = {id: 1, name: "user1", utilization: "30%"}
+let user2 = {id: 2, name: "user2", utilization: "40%"}
+let user3 = {id: 3, name: "user3", utilization: "0%"}
+let users = [user1, user2, user3]
 
 export class ProjectsPage extends React.Component {
-    constructor(props) {
-        super(props);
+
+    constructor() {
+        super();
+        this.state = {
+            data: projects,
+            expandedRows: []
+        };
     }
 
-    render() {
-        return (
-            <>
-                <h1>PROJECTS</h1>
-                <hr/>
-                <table id='t01'>
-                    <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Status</th>
-                        <th>Location</th>
-                        <th>StartDate</th>
-                        <th>EndDate</th>
-                        <th>Utilization</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {projects.map(project => (
-                        <ProjectTableRow project={project}/>
-                    ))}
-                    </tbody>
-                </table>
-            </>
-        );
-    };
-}
+    //todo: call back end function to get users info
+    getUsersByProjectID(id) {
+        return users;
+    }
 
-class ProjectTableRow extends React.Component {
-    render() {
-        const {project} = this.props;
-        return (
-            <tr key={project.id}>
-                <td>
-                    <button type="button" className="collapsible">{project.id}</button>
-                </td>
-                <td>
-                    <a href={"/projects/" + project.name}>{project.name}</a>
-                </td>
+
+    handleRowClick(rowId) {
+        const currentExpandedRows = this.state.expandedRows;
+        const isRowCurrentlyExpanded = currentExpandedRows.includes(rowId);
+
+        const newExpandedRows = isRowCurrentlyExpanded ?
+            currentExpandedRows.filter(id => id !== rowId) :
+            currentExpandedRows.concat(rowId);
+
+        this.setState({expandedRows: newExpandedRows});
+    }
+
+    renderProject(project) {
+        const clickCallback = () => this.handleRowClick(project.id);
+        const projectRows = [
+            <tr onClick={clickCallback} key={"row-data-" + project.id}>
+                <td>{project.id}</td>
+                {/*<td><a href={"/projects/" + project.name}>{project.name}</a></td>*/}
+                <td>{project.name}</td>
                 <td>{project.status}</td>
                 <td>{project.location}</td>
                 <td>{project.startDate}</td>
                 <td>{project.endDate}</td>
                 <td>{project.utilization}</td>
             </tr>
-        );
+        ];
 
+        const users = this.getUsersByProjectID(project.id);
+
+        if (this.state.expandedRows.includes(project.id)) {
+            projectRows.push(
+                <tr key={"row-expanded-" + project.id}>
+                    <td colSpan={7}>
+                        <ProjectInfoPage project={project} users={users}/>
+                    </td>
+                </tr>
+            );
+        }
+
+        return projectRows;
+    }
+
+
+    render() {
+        let allProjectRows = [];
+
+        this.state.data.forEach(project => {
+            const perProjectRows = this.renderProject(project);
+            allProjectRows = allProjectRows.concat(perProjectRows);
+        });
+
+        return (
+            <div className="container-project">
+                <div className="d1">1</div>
+                <div className="d2">
+                    <h1 >PROJECTS</h1>
+
+                    <hr/>
+                    <table id="t01">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Status</th>
+                            <th>Location</th>
+                            <th>StartDate</th>
+                            <th>EndDate</th>
+                            <th>Utilization</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {allProjectRows}
+                        </tbody>
+                    </table>
+                </div>
+                <div class="d1">3</div>
+            </div>
+
+
+        );
     }
 }
 
