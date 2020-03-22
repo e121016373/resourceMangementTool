2ad8b6976c97aade2eba7a3056eba8efeefa2ff8
eddhuang <eddhuang@students.cs.ubc.ca>
2020-03-22 14:38:34 -0700

    update projectStatus; update seed; add json to project hours

--- src/svc-dotnetcore3/Database/dbo/Tables/ProjectStatus.sql
+++ src/svc-dotnetcore3/Database/dbo/Tables/ProjectStatus.sql
@@ -1,20 +1,9 @@
 CREATE TABLE [dbo].[ProjectStatus](
-	[Id] INT NOT NULL,
-   [FromDate] DATETIME2 NOT NULL DEFAULT CONVERT(date, SYSUTCDATETIME()), 
-   [ToDate] DATETIME2 NOT NULL DEFAULT CONVERT(date, SYSUTCDATETIME()),
-   [status] VARCHAR(10) CHECK (status IN ('Active', 'Inactive', 'Forecast')),
- --  [Status] ENUM('Active', 'Inactive', 'Forecast')
- CONSTRAINT [PK_ProjectsId] PRIMARY KEY ([Id]),
-  CONSTRAINT [FK_ProjectsId] FOREIGN KEY ([Id]) REFERENCES [Projects]([Id])
+	[Id] INT NOT NULL UNIQUE,
+  [FromDate] DATE NOT NULL DEFAULT CONVERT(date, SYSUTCDATETIME()), 
+  [ToDate] DATE NOT NULL DEFAULT CONVERT(date, SYSUTCDATETIME()),
+  [Status] VARCHAR(10) CHECK ([Status] IN ('Active', 'Inactive', 'Forecast')),
+	[Hours] nvarchar(4000) NOT NULL
+CONSTRAINT [PK_ProjectsStatus] PRIMARY KEY ([Id]),
+CONSTRAINT [FK_ProjectsStatus_Projects] FOREIGN KEY ([Id]) REFERENCES [Projects]([Id]) ON DELETE CASCADE
 )
-
--- CREATE TABLE [dbo].[UserInProjects](
--- 	[UserId] INT NOT NULL,
--- 	[ProjectId] INT NOT NULL,
--- 	[FromDate] DATETIME2 NOT NULL DEFAULT CONVERT(date, SYSUTCDATETIME()),
--- 	[ToDate] DATETIME2 NOT NULL DEFAULT CONVERT(date, SYSUTCDATETIME()),
--- 	[Hours] INT NOT NULL
--- CONSTRAINT [PK_UserInProjects] PRIMARY KEY ([UserId], [ProjectId]),
--- CONSTRAINT [FK_UserInProjects_Users] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id]),
--- CONSTRAINT [FK_UserInProjects_Projects] FOREIGN KEY ([ProjectId]) REFERENCES [Projects]([Id]) 
--- )
