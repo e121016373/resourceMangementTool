3dca30b127b74ee9972b7f06396930005533e8fb
eddhuang <eddhuang@students.cs.ubc.ca>
2020-03-26 16:35:32 -0700

    new projectStatus table

--- src/svc-dotnetcore3/Database/dbo/Tables/ProjectStatus.sql
+++ src/svc-dotnetcore3/Database/dbo/Tables/ProjectStatus.sql
@@ -1,9 +1,21 @@
 CREATE TABLE [dbo].[ProjectStatus](
-	[Id] INT NOT NULL UNIQUE,
+	[Id] INT NOT NULL,
   [FromDate] DATE NOT NULL DEFAULT CONVERT(date, SYSUTCDATETIME()), 
   [ToDate] DATE NOT NULL DEFAULT CONVERT(date, SYSUTCDATETIME()),
-  [Status] VARCHAR(10) CHECK ([Status] IN ('Active', 'Inactive', 'Forecast')),
-	[Hours] nvarchar(4000) NOT NULL
-CONSTRAINT [PK_ProjectsStatus] PRIMARY KEY ([Id]),
+  [Status] NVARCHAR(10) CHECK ([Status] IN ('Active', 'Inactive', 'Forecast')),
+  [Year] [int] NOT NULL,
+	[Jan] [int] NOT NULL,
+	[Feb] [int] NOT NULL,
+	[Mar] [int] NOT NULL,
+	[Apr] [int] NOT NULL,
+	[May] [int] NOT NULL,
+	[Jun] [int] NOT NULL,
+	[Jul] [int] NOT NULL,
+	[Aug] [int] NOT NULL,
+	[Sep] [int] NOT NULL,
+	[Oct] [int] NOT NULL,
+	[Nov] [int] NOT NULL,
+	[Dec] [int] NOT NULL
+CONSTRAINT [PK_ProjectsStatus] PRIMARY KEY ([Id], [Year]),
 CONSTRAINT [FK_ProjectsStatus_Projects] FOREIGN KEY ([Id]) REFERENCES [Projects]([Id]) ON DELETE CASCADE
 )
