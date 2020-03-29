5f897269c5c37934a7902a61794419087dce0a53
shuaiqiz <shuaiqizhangwu@gmail.com>
2020-03-28 21:17:19 -0700

    delete duplicates

--- src/ui-react-client/src/components/projects/ProjectsPage.js
+++ src/ui-react-client/src/components/projects/ProjectsPage.js
@@ -81,25 +81,12 @@ class ProjectsPage extends React.Component {
         return users;
     }
 
-    // componentDidUpdate(prevProps,prevStates){
-    //     console.log('run to componentdidmount');
-    //     if (this.state.data.length ===0) {
-    //         console.log('before loadprojects');
-    //         loadProjects().catch(error => {
-    //             alert("load projects failed"+error);
-    //         })
-    //     }
-    // }
     getproject() {
-        // console.log('before load');
-        // console.log(loadProjects);
         if (this.props.projects.length === 0) {
             this.props.loadProjects().catch(error => {
                 alert("load projects failed" + error);
             });
         }
-        // console.log("IN props !!!! projects are following");
-        // console.log(this.props.projects);
     }
 
 
@@ -159,8 +146,6 @@ class ProjectsPage extends React.Component {
             });
         }
 
-
-
         return (
             <div>
                 <div style={{
@@ -191,7 +176,6 @@ class ProjectsPage extends React.Component {
                     </tr>
                     </thead>
                     <tbody>
-                    {/*{allProjectRows}*/}
                     {this.props.projects.length === 0 ? "loading": allProjectRows}
                     </tbody>
                 </table>
@@ -222,150 +206,5 @@ export default connect(
     mapStateToProps,
     mapDispatchToProps,
 )(ProjectsPage);
-
-// function ProjectsPage(props) {
-//
-//
-//     // constructor(props) {
-//     //     super(props);
-//     //     this.state = {
-//     //         data: projects,
-//     //         expandedRows: []
-//     //     };
-//     // }
-//     const [projectsObject, setProject] = useState({
-//         data: projects,
-//         expandedRows: []
-//     });
-//
-//     const getUsersByProjectID = (id) => {
-//         return users;
-//     };
-//
-//     const handleRowClick = (rowId) => {
-//         const currentExpandedRows = projectsObject.expandedRows;
-//         const isRowCurrentlyExpanded = currentExpandedRows.includes(rowId);
-//
-//         const newExpandedRows = isRowCurrentlyExpanded ?
-//             currentExpandedRows.filter(id => id !== rowId) :
-//             currentExpandedRows.concat(rowId);
-//
-//         setProject({expandedRows: newExpandedRows});
-//     };
-//
-//     const renderProject = (project) => {
-//         const clickCallback = () => handleRowClick(project.id);
-//         const projectRows = [
-//             <tr onClick={clickCallback} key={"row-data-" + project.id}>
-//                 <th>{project.id}</th>
-//                 {/*<td><a href={"/projects/" + project.name}>{project.name}</a></td>*/}
-//                 <td>{project.name}</td>
-//                 <td>{project.status}</td>
-//                 <td>{project.location}</td>
-//                 <td>{project.startDate}</td>
-//                 <td>{project.endDate}</td>
-//                 <td>{project.utilization}</td>
-//             </tr>
-//         ];
-//
-//         const users = getUsersByProjectID(project.id);
-//
-//         if (projectsObject.expandedRows.includes(project.id)) {
-//             projectRows.push(
-//                 <tr key={"row-expanded-" + project.id}>
-//                     <td colSpan={7}>
-//                         <ProjectInfoPage project={project} users={users}/>
-//                     </td>
-//                 </tr>
-//             );
-//         }
-//
-//         return projectRows;
-//     };
-//
-//
-//
-//     const constructRows = ()=>{
-//         let allProjectRows = [];
-//         projectsObject.data.forEach(project => {
-//             const perProjectRows = renderProject(project);
-//             allProjectRows = allProjectRows.concat(perProjectRows);
-//         });
-//         return allProjectRows;
-//     }
-//
-//     return (
-//         <div>
-//             <div style={{
-//                 display: 'flex',
-//                 'background-color': '#0f6674',
-//             }} className="container-projectheadFlex">
-//                 <div style={{
-//                     'background-color': "#FFDBC8",
-//                     'padding-top': '10px',
-//                     'padding-bottom': '10px',
-//                     'padding-left': '30px',
-//                     'padding-right': '30px',
-//                 }} className='createButton'>
-//                     <CreateProject/>
-//                 </div>
-//             </div>
-//             <hr/>
-//             <table className="table">
-//                 <thead>
-//                 <tr>
-//                     <th scope="col">ID</th>
-//                     <th scope="col">Name</th>
-//                     <th scope="col">Status</th>
-//                     <th scope="col">Location</th>
-//                     <th scope="col">StartDate</th>
-//                     <th scope="col">EndDate</th>
-//                     <th scope="col">Utilization</th>
-//                 </tr>
-//                 </thead>
-//                 <tbody>
-//                 {constructRows()}
-//                 </tbody>
-//             </table>
-//         </div>
-//     );
-//
-// }
-//
-//
-// ProjectsPage.propTypes = {
-//     projects: PropTypes.array.isRequired,
-//     locations: PropTypes.array.isRequired,
-//     loadProjects: PropTypes.func.isRequired,
-//     loadLocations: PropTypes.func.isRequired,
-// };
-//
-// const mapStateToProps = state => {
-//     return {
-//         projects:
-//             state.locations.length === 0
-//                 ? []
-//                 : state.projects.map(project => {
-//                     return {
-//                         ...project,
-//                         locationName: state.locations.find(
-//                             element => element.id === project.locationId,
-//                         ).name,
-//                     };
-//                 }),
-//         locations: state.locations,
-//     };
-// };
-//
-// const mapDispatchToProps = {
-//     loadProjects,
-//     loadLocations,
-// };
-//
-// export default connect(
-//     mapStateToProps,
-//     mapDispatchToProps,
-// )(ProjectsPage);
-
 
 
