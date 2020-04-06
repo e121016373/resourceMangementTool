3e6c7198afdcba9644edee4ae8e3ca29719e78d7
Bob Ghosh <ghosh.bob@gmail.com>
2020-04-05 20:02:49 -0700

    Projects Updated

--- src/svc-dotnetcore3/svc-dotnetcore3/Controllers/AdminController.cs
+++ src/svc-dotnetcore3/svc-dotnetcore3/Controllers/AdminController.cs
@@ -8,7 +8,7 @@ using Web.API.Application.Repository;
 
 namespace Web.API.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class AdminController : ControllerBase
     {
         private readonly IAdminRepository adminRepository;
