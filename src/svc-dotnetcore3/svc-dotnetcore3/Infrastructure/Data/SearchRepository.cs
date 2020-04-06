a0cb4613f2d9e73214fb37ded7ce7130c8120087
eddhuang <eddhuang@students.cs.ubc.ca>
2020-04-05 19:10:17 -0700

    search result show user without UH table

--- src/svc-dotnetcore3/svc-dotnetcore3/Infrastructure/Data/SearchRepository.cs
+++ src/svc-dotnetcore3/svc-dotnetcore3/Infrastructure/Data/SearchRepository.cs
@@ -21,83 +21,121 @@ namespace Web.API.Infrastructure.Data
         public async Task<IEnumerable<UserInSearch>> GetAllUsers(Search search)
         {
             var sql = @"
+                DECLARE @searchUser TABLE (
+                    Id INT, 
+                    Username NVARCHAR(50),
+                    Location NVARCHAR(100),
+                    Type NVARCHAR(50),
+                    Discipline NVARCHAR(100),
+                    Skill NVARCHAR(100),
+                    YOE NVARCHAR(50)
+                );
+
+                INSERT INTO @searchUser
+                SELECT DISTINCT
+                    U.Id, U.Username,
+                    L.Name AS 'Location', U.Type, D.Name AS 'Discipline', S.Name AS 'Skill',
+                    UWD.Year AS 'YOE'
+
+                FROM 
+                    Users U";
+
+            if (search.Skill != null) {
+                sql += @" 
+                    LEFT JOIN UserWorksDiscipline UWD ON U.Id = UWD.UserId
+                    LEFT JOIN UserHasSkills UHS ON U.Id = UHS.UserId";
+            } else if (search.Discipline != null) {
+                sql += @"
+                    LEFT JOIN UserWorksDiscipline UWD ON U.Id = UWD.UserId
+                    LEFT JOIN (
+                        SELECT UHS.UserId, MIN(UHS.SkillId) AS 'SkillId'
+                        FROM UserHasSkills UHS
+                        GROUP BY UHS.UserId
+                        ) UHS ON UHS.UserId = U.Id";
+            } else {
+                sql += @"
+                    LEFT JOIN (
+                        SELECT UWD.UserId, MIN(UWD.DisciplineId) AS 'DisciplineId', UWD.Year
+                        FROM UserWorksDiscipline UWD
+                        GROUP BY UWD.UserId, UWD.Year
+                        ) UWD ON UWD.UserId = U.Id
+                    LEFT JOIN (
+                        SELECT UHS.UserId, MIN(UHS.SkillId) AS 'SkillId'
+                        FROM UserHasSkills UHS
+                        GROUP BY UHS.UserId
+                        ) UHS ON UHS.UserId = U.Id";
+            }
+
+            sql += @"
+                    LEFT JOIN Locations L ON U.LocationId = L.Id
+                    LEFT JOIN Disciplines D ON UWD.DisciplineId = D.Id
+                    LEFT JOIN Skills S ON S.Id = UHS.SkillId AND S.DisciplineId = D.Id
+                WHERE    ";
+
+            if ((search.Discipline != null) && search.Discipline.Trim() != "") {
+                sql += @" 
+                    RTRIM(LTRIM(D.Name)) = LTRIM(@Discipline) AND";
+            }
+            if (search.Skill != null && search.Skill.Trim() != "") {
+                sql += @" 
+                    RTRIM(LTRIM(S.Name)) = LTRIM(@Skill) AND";
+            }
+            if (search.Location != null && search.Location.Trim() != "") {
+                sql += @" 
+                    RTRIM(LTRIM(L.Name)) = LTRIM(@Location) AND";
+            }
+            if (search.YOE != null) {
+                sql += @" 
+                    RTRIM(LTRIM(UWD.Year)) = LTRIM(@YOE) AND";
+            }
+
+            // remove the last AND
+            sql = sql.Substring(0, sql.Length-3);
+
+            sql += @"
+
                 SELECT *
                 FROM (
-                    SELECT DISTINCT
-                        U.Id, U.FirstName, U.LastName, U.Username, L.Name AS 'Location', 
-                        U.Type, D.Name AS 'Discipline', S.Name AS 'Skill', UWD.Year AS 'YOE'";
+                    SELECT *";
 
             if (search.FromDate != null && search.ToDate != null) {
                 int month_diff = ((search.ToDate.Year - search.FromDate.Year) * 12) + search.ToDate.Month - search.FromDate.Month;
                 int year = search.FromDate.Year;
                 int month = search.FromDate.Month;     
-                sql += ", (SELECT CONVERT(FLOAT, 1-SUM(UH.Hours/176.0/" + (month_diff + 1) + "))";
-                sql += @" 
-                    FROM UserHours UH
-                    WHERE UH.UserId = U.Id";
-                sql += " \nAND ((UH.Year = " + year + "AND UH.Month =" + month + ")";
+                sql += @", ISNULL((
+                        SELECT CONVERT(FLOAT, 1-SUM(UH.Hours/176.0/" + (month_diff + 1) + "))" + @"
+                        FROM UserHours UH
+                        WHERE
+                            U.Id = UH.UserId
+                            AND ((UH.Year = " + year + " AND UH.Month = " + month + ")";
                 month++;
                 for (int i = 0; i < month_diff; i++) {
                     if (month >= 13) {
                         year += 1;
                         month = 1;
                     }
-                    sql += " \nOR (UH.Year = " + year + " AND UH.Month = " + month + ")";
+                    sql += @"
+                                OR (UH.Year = " + year + " AND UH.Month = " + month + ")";
                     month++;
                 }
                 sql += @"
-                        ) 
-                    GROUP BY UH.UserId) AS 'Availability'";
+                            ) 
+                        GROUP BY UH.UserId), 1) AS 'Availability'";
             }
 
             sql += @"
-                    FROM 
-                        Users U
-                        LEFT JOIN UserWorksDiscipline UWD ON U.Id = UWD.UserId";
-
-            if (search.Skill != null) {
-                sql += " LEFT JOIN UserHasSkills UHS ON U.Id = UHS.UserId";
-            } else {
-                sql += @"
-                    LEFT JOIN (
-                        SELECT UHS.UserId, MIN(UHS.SkillId) AS 'SkillId'
-                        FROM UserHasSkills UHS
-                        GROUP BY UHS.UserId
-                        ) UHS ON UHS.UserId = U.Id
-            ";
-            }
-
-            sql += @"
-                LEFT JOIN Locations L ON U.LocationId = L.Id
-                LEFT JOIN Disciplines D ON UWD.DisciplineId = D.Id
-                LEFT JOIN Skills S ON UHS.SkillId = S.Id AND S.DisciplineId = D.Id
-            WHERE    ";
-
-            if ((search.Discipline != null) && search.Discipline.Trim() != "") {
-                sql += " RTRIM(LTRIM(D.Name)) = LTRIM(@Discipline) AND";
-            }
-            if (search.Skill != null && search.Skill.Trim() != "") {
-                sql += " RTRIM(LTRIM(S.Name)) = LTRIM(@Skill) AND";
-            }
-            if (search.Location != null && search.Location.Trim() != "") {
-                sql += " RTRIM(LTRIM(L.Name)) = LTRIM(@Location) AND";
-            }
-            if (search.YOE != null) {
-                sql += " RTRIM(LTRIM(UWD.Year)) = LTRIM(@YOE) AND";
-            }
-
-            // remove the last AND
-            sql = sql.Substring(0, sql.Length-3);
+                    FROM @searchUser U
+                ) AS SearchUser";
             
-            sql += " ) AS SearchUser";
             if (search.FromDate != null && search.ToDate != null) {
-                sql += " WHERE SearchUser.Availability >= @Availability/100.0";
+                sql += @" 
+                WHERE SearchUser.Availability >= @Availability/100.0";
             }
            
             sql += ";";
             
             // for debug
-            // Console.WriteLine(sql);
+            Console.WriteLine(sql);
 
             using var connection = new SqlConnection(connectionString);
             connection.Open();
